6ee98d7117b69bb05ffe9607732b68df8837c332
Waldemar <waldemar.schwan@uswitch.com>
2019-01-18 09:35:54 +0000

    refactor

--- src/dnd_roller/core.cljs
+++ src/dnd_roller/core.cljs
@@ -1,11 +1,7 @@
 (ns dnd-roller.core
   (:require [reagent.core :as r]))
 
-;; define your app data so that it doesn't get over-written on reload
-
-(defonce app-state (r/atom {:text "Hello world!"}))
-(defonce dice-selection (r/atom []))
-
+(defonce rolls (r/atom []))
 (defonce dice-selection-reset-timer (r/atom 0))
 
 (def dice-roll-sounds ["/audio/dice roll 1.m4a"
@@ -41,23 +37,20 @@
   [x]
   (max 0 (dec x)))
 
-(def d4  {:sides  4 :img "/img/dice/d4.png"})
-(def d6  {:sides  6 :img "/img/dice/perspective-dice-six.png"})
-(def d8  {:sides  8 :img "/img/dice/dice-eight-faces-eight.png"})
-(def d10 {:sides 10 :img "/img/dice/d10.png"})
-(def d12 {:sides 12 :img "/img/dice/d12.png"})
-(def d20 {:sides 20 :img "/img/dice/dice-twenty-faces-twenty.png"})
+(def d4   {:sides  4 :img "/img/dice/d4.png"})
+(def d6   {:sides  6 :img "/img/dice/perspective-dice-six.png"})
+(def d8   {:sides  8 :img "/img/dice/dice-eight-faces-eight.png"})
+(def d10  {:sides 10 :img "/img/dice/d10.png"})
+(def d12  {:sides 12 :img "/img/dice/d12.png"})
+(def d20  {:sides 20 :img "/img/dice/dice-twenty-faces-twenty.png"})
+(def d100 {:sides 20 :img "/img/dice/d10.png"})
 
-(def  all-dice [d4 d6 d8 d10 d12 d20])
+(def all-dice [d4 d6 d8 d10 d12 d20])
 
 (defn roll-dice
   [d]
   (assoc d :value
          (inc (rand-int (:sides d)))))
-
-(defn roll-dice-selection!
-  [& args]
-  (swap! dice-selection #(map roll-dice %)))
 
 (defn <dice>
   [attr dice]
@@ -74,14 +67,14 @@
                       :animation-iteration-count "infinite"
                       :animation-duration "0.5s"}
               :on-click (fn [event]
-                                                  ;;(some-> event .-target .-style .-animation-play-state println)
+                          ;;(some-> event .-target .-style .-animation-play-state println)
                           (let [target (some-> event .-target)]
                             (some-> target .-style (.setProperty "animation-play-state" "running" ""))
                             (js/setTimeout
-                             #(some-> target .-style (.setProperty "animation-play-state" "paused" ""))
-                             500))
+                              #(some-> target .-style (.setProperty "animation-play-state" "paused" ""))
+                              500))
                           (play-random-dice-sound)
-                          (swap! dice-selection conj
+                          (swap! rolls conj
                                  (-> d
                                      (assoc :id (gensym))
                                      roll-dice))
@@ -90,16 +83,17 @@
 
 (defn <dice-tower>
   []
-  (let [rolles (keep :value @dice-selection)
-        min-v (reduce min rolles)
-        max-v (reduce max rolles)]
+  (let [rolles (keep :value @rolls)
+        ;min-v  (reduce min rolles)
+        ;max-v  (reduce max rolles)
+        ]
     (if (empty? rolles)
       [:div]
       [:div
-       (for [d @dice-selection]
+       (for [d @rolls]
          [:span {:key (:id d)
                  :style {:display "inline-block"}
-                 :on-click #(swap! dice-selection
+                 :on-click #(swap! rolls
                                    (fn [ds]
                                      (remove
                                       (fn [dd] (= (:id d) (:id dd)))
@@ -111,8 +105,9 @@
                :class (cond
                         (= 1 (:value d))          "crit-fail"
                         (= (:sides d) (:value d)) "crit-roll"
-                        (= min-v (:value d))      "worst-roll"
-                        (= max-v (:value d))      "best-roll")}
+                        ;(= min-v (:value d))      "worst-roll"
+                        ;(= max-v (:value d))      "best-roll"
+                        )}
            (or (:value d) "?")]])
        [:span {:style {:display "inline-block"}}
         [:p {:style {:text-align "center"
@@ -143,7 +138,7 @@
   (js/setInterval #(do
                      (swap! dice-selection-reset-timer count-down)
                      (when (zero? @dice-selection-reset-timer)
-                       (reset! dice-selection [])))
+                       (reset! rolls [])))
                   1000)
   (start))
 
